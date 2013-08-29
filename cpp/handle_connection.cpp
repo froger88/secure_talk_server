9363ae1b7d0c5081d5a64c4b27ffca17c0a4145b
froger88 <froger@froger.pl>
2013-08-29 02:35:59 +0200

    adding seal, start with implementation of protocol

--- cpp/handle_connection.cpp
+++ cpp/handle_connection.cpp
@@ -1,5 +1,48 @@
 #include "handle_connection.h"
 
+/*
+                    +IMMMM .~MMZ.
+                 .MM NMMMMM  .MMMM
+                MMM. MMMMMMZ   MMMM.
+              .MMM, .MMMMMMM  ..MMMM
+              .MMM. ZMMMMMMM.   MMMM.
+              .MMM  =MMMMMMM.   MMMM.
+              .MMM . MMMMMMM.  MMMM
+                MMM: MMMMMMM .ZMMM
+                  MMM MMMMMM.~MO
+                      ~MMMN..   ...M.
+                        .?M8 .. +.NI
+                       . .....  MNM D
+                        : D..Z...MO.?.
+                          NM . M..  .~
+                         .~I...     .,
+                          .M.       M.
+                         .M.        :
+                        .M        .MM
+                       .7           M.
+                       M            MO
+                      M.            .8       .=MMMMMMM .
+                     M.             .I    MM$          ,M
+                    .                M MM .             .M
+                    M                 N              .   M
+                   .:                                M   ,.
+                                                     +   .
+                    ,                                Z  .M
+                   .M                               ..  ,
+                    M                  .            M.  M..  =+, .
+                     M        ?        D           :+   7  ..M$ ..
+                     .Z        M.       ,         DMM.     .. =M.
+                      ,M      .8,       M        M.. .MMM,...
+               .. N  M$ MI.    MM.      :.   ..M$
+             .$...  =  MM . D,,MM7       MMMM,
+                   Z...    MN   MM.      MMOMMM7..
+                      .D.,8      M.      :..:NM$
+                                 MM.      .MM~.,
+                                  MM.  ~=7DMMM$.
+
+          S   E   A    L       O   F      T   H   E       D   A   Y
+*/
+
 using namespace std;
 
 void handle_connection(int fd, SSL* ssl, SecureCounter* cnt_handle_connection)
@@ -26,10 +69,15 @@ void handle_connection(int fd, SSL* ssl, SecureCounter* cnt_handle_connection)
 		count = SSL_read(ssl, buf, sizeof (buf)); /* get request */
 		if (count > 0) {
 			buf[count] = 0;
-			cout << "Client msg: " << buf;
+			cerr << "Client msg count: " << count << endl <<flush;
+			cerr << "Client msg: " <<flush;
+			for(int i=0; i< count; ++i) {
+				cerr << (int)(unsigned char)buf[i] << " " <<flush;
+			}
+			cerr << endl << flush;
 			// answer with the same
 			cerr << "handle_connection SSL_write" << endl << flush;
-			SSL_write(ssl, buf, strlen(buf));
+			SSL_write(ssl, buf, count);
 		} else {
 			// errors occured
 			goto cleanup;
